83dae9da36a43f448b307b13058a4eb13fe4c187
Neil <neil12wipro@gmail.com>
2023-09-19 20:38:15 +0530

    certificates added

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,21 +5,31 @@ import ProjectCategory from './ProjectCategory.js';
 import NotesCategory from './NotesCategory.js';
 import { project_categories, projects } from '../data/projects_and_categories.js';
 import { notes_categories, notes } from '../data/notes_and_categories.js';
+import { certificates } from '../data/certificates.js';
 
 const Home = () => {
 
   const [showProjects, setShowProjects] = useState(true); // Initially, show projects
   const [showNotes, setShowNotes] = useState(false);
+  const [showCertificates, setShowCertificates] = useState(false);
 
   // Define functions to handle button clicks
   const handleShowProjectsClick = () => {
     setShowProjects(true);
     setShowNotes(false);
+    setShowCertificates(false);
   };
 
   const handleShowNotesClick = () => {
-    setShowProjects(false);
     setShowNotes(true);
+    setShowProjects(false);
+    setShowCertificates(false);
+  };
+
+  const handleShowCertificatesClick = () => {
+    setShowCertificates(true);
+    setShowProjects(false);
+    setShowNotes(false);
   };
 
   return (
@@ -43,6 +53,14 @@ const Home = () => {
             onClick={handleShowNotesClick}
           >
             Notes
+          </button>
+          <button
+            className={`${
+              showCertificates ? 'bg-active text-white hover:bg-[#529E4E]' : 'bg-secondary text-gray-400 hover:bg-[#1D222B] hover:text-gray-200'
+            } px-4 py-2 rounded`}
+            onClick={handleShowCertificatesClick}
+          >
+            Certificates
           </button>
         </div>
 
@@ -71,6 +89,20 @@ const Home = () => {
                 notes={category.notes}
               />
             ))}   
+          </div>
+        )}
+
+        {showCertificates && (
+          <div >
+            <p>Total: {Object.keys(certificates).length}</p>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-4 w-full'>
+              {certificates.map((certificate) => (
+                <div className='mb-8'>
+                  <h2 className='text-2xl font-bold mb-4'>{certificate.name}</h2>
+                  <img src={certificate.image} alt={certificate.name} />
+                </div>
+              ))}  
+            </div> 
           </div>
         )}
 
---
+++ src/data/certificates.js
@@ -0,0 +1,19 @@
+import NeuralNetworksImg from '../assets/neural-networks.jpg';
+import GenerativeAICertficateImg from '../assets/coursera/certificate-generative-ai-with-large-language-models.jpg';
+import NeuralNetworksCertificateImg from '../assets/coursera/certificate-neural-networks-and-deep-learning.jpg';
+
+
+export const certificates = [
+    // Generative AI with LLM
+    {
+        name: 'Generative AI with LLM',
+        image: GenerativeAICertficateImg
+    },
+
+    // Neural Networks and Deep Learning
+    {
+        name: 'Neural Networks and Deep Learning',
+        image: NeuralNetworksCertificateImg
+    }
+
+];
