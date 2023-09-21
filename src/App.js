4ca1e1995857cea71ec0ce435dbacd9ba814c462
Neil <neil12wipro@gmail.com>
2023-09-21 11:54:17 +0530

    Added HashRouter for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import ProjectDetails from './components/ProjectDetails';
 import NotesDetails from './components/NotesDetails';
