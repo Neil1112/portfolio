5e11ba793eba49f44350ec0444e76d4781b0339f
Neil <neil12wipro@gmail.com>
2023-09-28 16:40:43 +0530

    added deep learning with pytorch - image segmentation certificate

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -2,7 +2,7 @@ import NeuralNetworksImg from '../assets/neural-networks.jpg';
 import GenerativeAICertficateImg from '../assets/coursera/certificate-generative-ai-with-llm.jpg';
 import NeuralNetworksCertificateImg from '../assets/coursera/certificate-neural-networks-and-deep-learning.jpg';
 import ConvolutionalNeuralNetworksCertificateImg from '../assets/coursera/certificate-convolutional-neural-networks.jpg';
-
+import DeepLearningWithPyTorchImageSegmentationCertificateImg from '../assets/coursera/certificate-deep-learning-with-pytorch-image-segmentation.jpg';
 
 export const certificates = [
     // Generative AI with LLM
@@ -21,6 +21,12 @@ export const certificates = [
     {
         name: 'Convolutional Neural Networks',
         image: ConvolutionalNeuralNetworksCertificateImg
+    },
+
+    // Deep Learning with PyTorch : Image Segmentation
+    {
+        name: 'Deep Learning with PyTorch : Image Segmentation',
+        image: DeepLearningWithPyTorchImageSegmentationCertificateImg
     }
 
 ];
